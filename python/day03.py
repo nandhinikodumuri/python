4dad86c3b4ff9dadfb58761b2f5940e8177882da
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-08-11 17:20:40 +0530

    Added day03

---
+++ python/day03.py
@@ -0,0 +1,25 @@
+num = 60
+if num % 2 == 0:
+     print("Even")
+else:
+     print("Odd")
+
+
+marks = 20
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+
+num = -5
+if num >= 0:
+    print("Positive number")
+else:
+    print("Negative number")
+
+
+age = 20
+if age >=18:
+    print("Eligible for vote")
+else:
+    print("Not eligible for vote")
