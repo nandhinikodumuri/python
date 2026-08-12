6867310aadb89c2b8d19ed55bebd0239b3f64584
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-08-12 20:22:54 +0530

    Added patterns.py

--- python/if,if-else.py
+++ python/if,if-else.py
@@ -1,7 +1,43 @@
-num1 = 100
-num2 = 50
+# num1 = 100
+# num2 = 50
 
-if num1 > num2:
-    print("Greater number is:", num1)
+# if num1 > num2:
+#     print("Greater number is:", num1)
+# else:
+#     print("Greater number is:", num2)
+
+# num = int(input("Entre a number:"))
+# if num % 5 == 0:
+#     print("Divisible by 5")
+# else:
+#     print("Not divisible by 5")
+
+
+# num = 101
+# if num % 3 == 0 and num % 5 ==  0:
+#     print("Divisible by both 3 and 5")
+# else:
+#     print("Not divisible by both 3 and 5")
+
+# char = "aeiou"
+# if char in "aeiouAEIOU":
+#     print("Character is vowel")
+# else:
+#     print("Character is consonant")
+
+# 
+# num = 5
+# if num == 0:
+#     print("number is zero")
+# else:
+#     print("number is non zero")
+
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+c = int(input("Enter a number:"))
+if a > b and a > c:
+    print("Greater number is :",a)
+elif b > a and b > c:
+    print("Greater number is :",b)
 else:
-    print("Greater number is:", num2)
+    print("Greater number is :",c)
---
+++ python/patterns.py
@@ -0,0 +1,95 @@
+#Square pattern
+# row = 5
+# for i in range (1, 6):
+#     for j in range(1,6):
+#         print("*", end=" ")
+#     print()
+
+#right traingle pattern
+# row = 5
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*", end=" ")
+#     print()
+
+#triangle pattern
+# row = 5
+# for i in range(1,6):
+#     for K in range(row-i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+#left triangle pattern
+# row = 4
+# for i in range(1,row+1):
+#     for K in range(row-i):
+#           print(" ", end="")   // if we  keep space it becomes  left triangle
+#     for j in range(i):
+#           print("*", end=" ")
+#     print()
+
+#reverse triangle pattern // reverse left triangle pattern
+# row = 5
+# for i in range(row,0,-1):
+#     for K in range(row-i):
+#           print(" ", end="")
+#     for j in range(i):
+#           print("*", end=" ")
+#     print()
+
+#diamond pattern
+# row =5 
+# for i in range(1,row+1):
+#     for k in range(row-i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(row,0,-1):
+#     for k in range(row-i):
+#         print(" ", end = "")
+#     for j in range(i):
+#         print("*", end = " ")
+#     print()
+
+#reverse diamond pattern
+# row = 5
+# for i in range(row,0,-1):
+#     for k in range(row-i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(1,row+1):
+#     for k in range(row-i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+#number pattern of right triangle
+row = 5
+for i in range(1,6):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+row = 5
+for i in range(1,6):
+    for j in range(i):
+        print(j+1, end=" ")
+    print()
+
+row = 5
+c = 1
+for i in range(1,6):
+    for k in range(row-i):
+        print(" ", end="")
+    for j in range(i):
+        print(c, end=" ")
+        c = c + 1
+    print()
