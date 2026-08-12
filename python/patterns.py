8d8fbe84341c475e3360dc03f9468a8a41ce2e4f
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-08-12 20:44:08 +0530

    Added patterns.py

--- python/patterns.py
+++ python/patterns.py
@@ -1,75 +1,75 @@
 #Square pattern
-# row = 5
-# for i in range (1, 6):
-#     for j in range(1,6):
-#         print("*", end=" ")
-#     print()
+row = 5
+for i in range (1, 6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()
 
 #right traingle pattern
-# row = 5
-# for i in range(1,6):
-#     for j in range(1,i+1):
-#         print("*", end=" ")
-#     print()
+row = 5
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print()
 
 #triangle pattern
-# row = 5
-# for i in range(1,6):
-#     for K in range(row-i):
-#         print(" ", end="")
-#     for j in range(i):
-#         print("*", end=" ")
-#     print()
+row = 5
+for i in range(1,6):
+    for K in range(row-i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
 #left triangle pattern
-# row = 4
-# for i in range(1,row+1):
-#     for K in range(row-i):
-#           print(" ", end="")   // if we  keep space it becomes  left triangle
-#     for j in range(i):
-#           print("*", end=" ")
-#     print()
+row = 4
+for i in range(1,row+1):
+    for K in range(row-i):
+          print(" ", end="")     #// if we  keep space it becomes  left triangle
+    for j in range(i):
+          print("*", end=" ")
+    print()
 
 #reverse triangle pattern // reverse left triangle pattern
-# row = 5
-# for i in range(row,0,-1):
-#     for K in range(row-i):
-#           print(" ", end="")
-#     for j in range(i):
-#           print("*", end=" ")
-#     print()
+row = 5
+for i in range(row,0,-1):
+    for K in range(row-i):
+          print(" ", end="")
+    for j in range(i):
+          print("*", end=" ")
+    print()
 
 #diamond pattern
-# row =5 
-# for i in range(1,row+1):
-#     for k in range(row-i):
-#         print(" ", end="")
-#     for j in range(i):
-#         print("*", end=" ")
-#     print()
+row =5 
+for i in range(1,row+1):
+    for k in range(row-i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
 # for i in range(row,0,-1):
-#     for k in range(row-i):
-#         print(" ", end = "")
-#     for j in range(i):
-#         print("*", end = " ")
-#     print()
+    for k in range(row-i):
+        print(" ", end = "")
+    for j in range(i):
+        print("*", end = " ")
+    print()
 
 #reverse diamond pattern
-# row = 5
-# for i in range(row,0,-1):
-#     for k in range(row-i):
-#         print(" ",end="")
-#     for j in range(i):
-#         print("*", end=" ")
-#     print()
+row = 5
+for i in range(row,0,-1):
+    for k in range(row-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
-# for i in range(1,row+1):
-#     for k in range(row-i):
-#         print(" ",end="")
-#     for j in range(i):
-#         print("*", end=" ")
-#     print()
+for i in range(1,row+1):
+    for k in range(row-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
 #number pattern of right triangle
 row = 5
@@ -93,3 +93,4 @@ for i in range(1,6):
         print(c, end=" ")
         c = c + 1
     print()
+
