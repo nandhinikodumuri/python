21c74cd2ba72c5c5d18bb2bd812d952c874ac10e
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 16:55:29 +0530

    day09.py

---
+++ python/day09.py
@@ -0,0 +1,19 @@
+# Strings
+name = "Sai Bhavaani"
+print(name,type(name))
+#Operations on Strings
+#string concatenation
+name1 = "Sai"
+name2 = "Bhavaani"
+result = name1 + " " + name2
+print(result)
+#string reputation
+print(name1*3)
+#Accessing characters in a string
+print(name[0])
+#slicing
+print(name[0:5:1])
+print(name[::1])
+#membership operator
+print("Priya" in name)
+print("Priya" not in name)
