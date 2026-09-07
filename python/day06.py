99f427ea1182c4e9d177cdf6f7f342612487fc6f
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 16:46:10 +0530

    day06.py

---
+++ python/day06.py
@@ -0,0 +1,17 @@
+#Type Casting
+#implict type casting
+a=10 #int
+b=20.5 #float
+print(a+b)
+# anotherexample
+s=True
+t=5
+print(s+t)
+#Explicit type casting
+p=100
+q=float(p)
+print(q)
+c=str(p)
+print(c)
+d=bool(p)
+print(a)
