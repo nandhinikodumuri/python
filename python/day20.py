2e652fc2b3a5e6e460f33310dcb33379a8e2e35d
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 17:25:37 +0530

    day20.py

--- python/day20.py
+++ python/day20.py
@@ -1,44 +1,16 @@
-num1 = 100
-num2 = 50
+#Factorial using recursion
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+print(factorial(5))
 
-if num1 > num2:
-    print("Greater number is:", num1)
-else:
-    print("Greater number is:", num2)
-
-num = int(input("Entre a number:"))
-if num % 5 == 0:
-    print("Divisible by 5")
-else:
-    print("Not divisible by 5")
-
-
-num = 101
-if num % 3 == 0 and num % 5 ==  0:
-    print("Divisible by both 3 and 5")
-else:
-    print("Not divisible by both 3 and 5")
-
-char = "aeiou"
-if char in "aeiouAEIOU":
-    print("Character is vowel")
-else:
-    print("Character is consonant")
-
-
-num = 5
-if num == 0:
-    print("number is zero")
-else:
-    print("number is non zero")
-
-
-a = int(input("Enter a number:"))
-b = int(input("Enter a number:"))
-c = int(input("Enter a number:"))
-if a > b and a > c:
-    print("Greater number is :",a)
-elif b > a and b > c:
-    print("Greater number is :",b)
-else:
-    print("Greater number is :",c)
+#Fibonacci Series using Recursion
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+for i in range(10):
+    print(fibonacci(i), end=" ")
