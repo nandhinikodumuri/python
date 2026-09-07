dfbb081d7f464b1708624396fe0022224c53fba3
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 17:22:02 +0530

    day16.py

--- python/day16.py
+++ python/day16.py
@@ -93,3 +93,79 @@ for i in range(1,6):
         print(c, end=" ")
         c = c + 1
     print()
+
+#Number Patterns
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+
+print("hello")
+
+r=4
+for i in range(1,r+1):
+    for j in range(i):
+        print(j,end=" ")
+    print()
+
+print("hello")
+
+r=4
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+print("hello")
+
+r=3
+c=1
+for i in range(1,r+1):
+    for j in range(i):
+        print(c,end=" ")
+        c=c+1
+    print()
+
+print("hello")
+
+r=3
+c=65
+for i in range(1,r+1):
+    for j in range(i):
+        print(chr(c),end=" ")
+        c=c+1
+    print()
+
+print("hello")
+
+#HELLOW PATTERNS
+
+r=4
+for i in range(r):
+    for j in range(r):
+        if i==0 or i==r-1 or j==0 or j==r-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+
+print("hello")
+
+r=5
+for i in range(r):
+    for j in range(r):
+        if i == r//2 or j==r/2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("hello")
+
+r=5 
+for i in range(r):
+    for j in range(r):
+        if i==j or i+j==r-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
