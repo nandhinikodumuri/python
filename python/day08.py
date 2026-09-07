d1893d9a93fd521d7fe021a456595bd9b6d75922
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 16:53:57 +0530

    day08.py

---
+++ python/day08.py
@@ -0,0 +1,29 @@
+#String input
+name=input("Enter your name: ")
+print(name)
+#Integer input
+num=int(input("Enter a number: "))
+print(num)
+#Float input
+price=float(input("Enter the price: "))
+print(price)
+#List of integers input
+list=[1,2,3,4,5]
+print(list)
+print(type(list))
+#Print()
+#print text
+print("Hello, World!")
+#print items
+name ="jhanu" 
+age = 25
+print(name ,age)
+#use of separator
+print('2000','06','05',sep='-')
+#use of end
+print('Hello',end=' ')
+print('World!')
+#f.string
+print(f"Name: {name}, Age: {age}")
+# % operator
+print("Name: %s, Age: %d" % (name, age))
