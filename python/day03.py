3840ab190cc6ceced07dff78e8aa4801b85ad6bc
nandhinikodumurigit config --global user.email <nandhinikodumuri@gmail.com>
2026-09-07 16:41:35 +0530

    day03.py

--- python/day03.py
+++ python/day03.py
@@ -56,3 +56,15 @@ print(list(numbers))
 # 14. Check Data Type
 x = 100
 print(type(x))
+
+#Assigning multiple values to multiple variables
+a,b,c=5,10,15
+print("Value of a:",a)
+print("Value of b:",b)
+print("Value of c:",c)
+
+#Assign One Value to Multiple Variables
+p=q=r=100
+print("Value of p:",p)
+print("Value of q:",q)
+print("Value of r:",r)
